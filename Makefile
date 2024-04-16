587eb1ea4ab985a37c31ebd86cdb7269f900c30e
whonion <root@whonion.app>
2024-04-16 10:49:11 +0800

    Initial commit

---
+++ Makefile
@@ -0,0 +1,45 @@
+GOCMD=go
+GOTEST=$(GOCMD) test
+GOVET=$(GOCMD) vet
+BINARY_NAME=go-tonwallet-batchgen
+VERSION?=1.0.0
+SERVICE_PORT?=3000
+DOCKER_REGISTRY?= #if set it should finished by /
+EXPORT_RESULT?=false # for CI please set EXPORT_RESULT to true
+
+build: ## Build your project and put the output binary in out/bin
+ifeq ($(OS),Windows_NT)
+	$(GOCMD) build -o $(BINARY_NAME).exe main.go
+else
+	$(GOCMD) build -o $(BINARY_NAME) main.go
+endif
+## Test:
+test: ## Run the tests of the project
+ifeq ($(EXPORT_RESULT), true)
+	GO111MODULE=off go get -u github.com/jstemmer/go-junit-report
+	$(eval OUTPUT_OPTIONS = | tee /dev/tty | go-junit-report -set-exit-code > junit-report.xml)
+endif
+	$(GOTEST) -v -race ./... $(OUTPUT_OPTIONS)
+
+coverage: ## Run the tests of the project and export the coverage
+	$(GOTEST) -cover -covermode=count -coverprofile=profile.cov ./...
+	$(GOCMD) tool cover -func profile.cov
+ifeq ($(EXPORT_RESULT), true)
+	GO111MODULE=off go get -u github.com/AlekSi/gocov-xml
+	GO111MODULE=off go get -u github.com/axw/gocov/gocov
+	gocov convert profile.cov | gocov-xml > coverage.xml
+endif
+
+## Lint:
+lint: lint-go lint-yaml ## Run all available linters
+
+lint-go: ## Use golintci-lint on your project
+	$(eval OUTPUT_OPTIONS = $(shell [ "${EXPORT_RESULT}" == "true" ] && echo "--out-format checkstyle ./... | tee /dev/tty > checkstyle-report.xml" || echo "" ))
+	docker run --rm -v $(shell pwd):/app -w /app golangci/golangci-lint:latest-alpine golangci-lint run --deadline=65s $(OUTPUT_OPTIONS)
+
+lint-yaml: ## Use yamllint on the yaml file of your projects
+ifeq ($(EXPORT_RESULT), true)
+	GO111MODULE=off go get -u github.com/thomaspoignant/yamllint-checkstyle
+	$(eval OUTPUT_OPTIONS = | tee /dev/tty | yamllint-checkstyle > yamllint-checkstyle.xml)
+endif
+	docker run --rm -it -v $(shell pwd):/data cytopia/yamllint -f parsable $(shell git ls-files '*.yml' '*.yaml') $(OUTPUT_OPTIONS)
---
+++ main.go
@@ -0,0 +1,114 @@
+package main
+
+import (
+	"context"
+	"encoding/json"
+	"fmt"
+	"log"
+	"os"
+	"strings"
+
+	"github.com/xssnick/tonutils-go/liteclient"
+	"github.com/xssnick/tonutils-go/ton"
+	"github.com/xssnick/tonutils-go/ton/wallet"
+)
+
+// WalletInfo represents the structure of wallet information to be saved
+type WalletInfo struct {
+	Seed        string `json:"seed"`
+	AddressV4R2 string `json:"addressV4R2"`
+}
+
+func main() {
+
+
+	client := liteclient.NewConnectionPool()
+
+	configUrl := "https://ton-blockchain.github.io/global.config.json"
+
+
+	err := client.AddConnectionsFromConfigUrl(context.Background(), configUrl)
+	if err != nil {
+		panic(err)
+	}
+	api := ton.NewAPIClient(client)
+
+	var numWallets int
+	fmt.Print("Enter the number of wallets to create: ")
+	_, err = fmt.Scan(&numWallets)
+	if err != nil {
+		log.Fatal("Failed to read input:", err)
+		return
+	}
+
+	done := make(chan struct{})
+
+	var walletInfos []WalletInfo
+
+	for i := 0; i < numWallets; i++ {
+		go func() {
+			defer func() {
+				done <- struct{}{}
+			}()
+
+			seed := wallet.NewSeed()
+			seedString := strings.Join(seed, " ") // Joining the slice into a single string
+			fmt.Println("Seed phrase:", seedString)
+
+			w, err := wallet.FromSeed(api, seed, wallet.V4R2) // Passing seed as slice of strings
+			if err != nil {
+				log.Println("FromSeed err:", err)
+				return
+			}
+
+			address := w.Address().String()
+
+			// Create WalletInfo struct
+			walletInfo := WalletInfo{
+				Seed:        seedString,
+				AddressV4R2: address,
+			}
+
+			// Append WalletInfo to slice
+			walletInfos = append(walletInfos, walletInfo)
+
+			fmt.Println("Wallet address:", address)
+
+			block, err := api.CurrentMasterchainInfo(context.Background())
+			if err != nil {
+				log.Println("CurrentMasterchainInfo err:", err)
+				return
+			}
+
+			balance, err := w.GetBalance(context.Background(), block)
+			if err != nil {
+				log.Println("GetBalance err:", err)
+				return
+			}
+
+			fmt.Println("Balance:", balance)
+		}()
+	}
+
+	for i := 0; i < numWallets; i++ {
+		<-done
+	}
+
+	// Save wallet information to JSON file
+	file, err := os.Create("wallets.json")
+	if err != nil {
+		log.Fatal("Failed to create file:", err)
+		return
+	}
+	defer file.Close()
+
+	encoder := json.NewEncoder(file)
+	encoder.SetIndent("", "    ")
+	err = encoder.Encode(walletInfos)
+	if err != nil {
+		log.Fatal("Failed to encode wallets to JSON:", err)
+		return
+	}
+
+	fmt.Println("Wallets information saved to wallets.json")
+}
